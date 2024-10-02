bb1cd0e7ef9920b05f7b6a85289081700179744f
Charleszbr <zobiricharles@gmail.com>
2024-10-02 14:28:22 +0200

    Normalize line endings

---
+++ src/Entity/ThemeImage.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ThemeImageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ThemeImageRepository::class)]
+class ThemeImage
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $theme = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $image_url = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTheme(): ?string
+    {
+        return $this->theme;
+    }
+
+    public function setTheme(string $theme): static
+    {
+        $this->theme = $theme;
+
+        return $this;
+    }
+
+    public function getImageUrl(): ?string
+    {
+        return $this->image_url;
+    }
+
+    public function setImageUrl(string $image_url): static
+    {
+        $this->image_url = $image_url;
+
+        return $this;
+    }
+}
