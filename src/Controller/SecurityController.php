ddb7700bb1a8ccc39057fd08be1005d981f20ce8
Charleszbr <zobiricharles@gmail.com>
2024-10-03 15:33:21 +0200

    visual of differents pages

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -18,6 +18,11 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
+        // If the user is already logged in, redirect to the homepage
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('../templates/home'); 
+        // }
+
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
