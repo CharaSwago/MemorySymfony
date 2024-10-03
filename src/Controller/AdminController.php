e60bbf3a7b2d77b3d77a8675f00037524a2bbe03
coding <coding@codings-MacBook-Pro.local>
2024-10-03 19:15:09 +0200

    hash password

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,39 +2,43 @@
 
 namespace App\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\User;
-use App\Form\AddPatientType;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Form\AddPatientType;
+
 
 
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
     public function index( EntityManagerInterface $entityManager): Response
-    {
-        
+    {        
         return $this->render('admin/index.html.twig', [
             'user' => $this->getUser(),
         ]);
     }
 
     #[Route('/admin/add/patient', name: 'app_admin_add_patient')]
-    public function addPatients(Request $request, EntityManagerInterface $entityManager): Response
+    public function addPatients(UserPasswordHasherInterface $userPasswordHasher, Security $security, Request $request, EntityManagerInterface $entityManager): Response
     {
-        $patient = new User();
+        $patient = new User();       
         $orth = $this->getUser();
-        $orth->addPatient($patient);
         $form = $this->createForm(AddPatientType::class, $patient);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             // $form->getData() holds the submitted values
             // but, the original `$task` variable has also been updated
-            $user = $form->getData();
+            $plainPassword = $form->get('plainPassword')->getData();
+            $patient = $form->getData();
+            $patient->setPassword($userPasswordHasher->hashPassword($patient, $plainPassword));        
             $entityManager->persist($user);
             $entityManager->flush();
 
